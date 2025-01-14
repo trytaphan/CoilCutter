8a6dedbfb0ed8939a7b9451a2d8276b7acf648aa
Xuan <893570476@qq.com>
2025-01-14 14:22:49 +0800

    测试了AgGrid

--- cutter.py
+++ cutter.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from st_aggrid import AgGrid, GridOptionsBuilder
 from support import SupportBracket
 
 st.title("Cutter")
@@ -11,12 +12,19 @@ df = pd.DataFrame(columns=SupportBracket.property_list)
 print(len(df.index))
 df.loc[len(df.index)] = a.to_dict()
 df.loc[len(df.index)] = b.to_dict()
-
-print(df)
-print(a.specification)
-print(a.specification_r)
-print(SupportBracket.display_in_CN(df))
-
 edited_df = st.data_editor(SupportBracket.display_in_CN(df))
+# 配置GridOptions
+gb = GridOptionsBuilder.from_dataframe(df)
+gb.configure_selection(selection_mode="multiple",    # 允许多选行
+                       use_checkbox=True)            # 加勾选框
+gb.configure_grid_options(enableRangeSelection=True)    # 框选多个单元格，支持shift和ctrl
+grid_options = gb.build()
 
+
+grid_response = AgGrid(df,
+                       gridOptions=grid_options,
+                       editable=True,
+                       height=100
+                       )
+# st.dataframe(df)
 
