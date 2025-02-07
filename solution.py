da49ff9fb056873d261b7a1c23ac3fea92612872
Xuan <893570476@qq.com>
2025-02-07 15:08:17 +0800

    正在重写规划算法中生成裁剪模式的算法

--- solution.py
+++ solution.py
@@ -3,70 +3,116 @@ import itertools
 import pandas as pd
 
 
+class CuttingPatterns:
+    def __init__(self, products):
+        self.raw_matrices = {}  # 存储不同原材料宽度的裁剪方案
+        self.products = products  # 记录成品信息
 
-def generate_cutting_patterns_without_redundancy(raw_materials, products):
-    """
-    生成无边丝（无冗余宽度）的裁剪方案
+    def print(self):
+        for raw_width, df in self.raw_matrices.items():
+            print(f"宽度为{raw_width}的原料裁剪方案为：")
+            print(df)
 
-    arguments：
-    - raw_materials: DataFrame，包含 "width"（原材料宽度）
-    - products: DataFrame，包含 "width"（成品宽度）
+    def generate(self, raw_materials, cost_df):
+        """
 
-    return：
-    - pattern_matrices: 一个列表，每个元素是一个 DataFrame（表示该原材料的所有裁剪方案）
-    """
-    max_counts = (raw_materials.width.values.reshape(-1, 1) //
-                  products.width.values.reshape(1, -1))
-    pattern_matrices = {}
-    for r in range(len(raw_materials.width)):
-        # 生成所有组合
-        temp = itertools.product(*[range(count + 1) for count in max_counts[r]])
-        temp_df = pd.DataFrame(temp, columns=products.width)
+        :param raw_materials: DataFrame, 有一列为可选原料的宽度
+        :param cost_df: DataFrame, 两列:start_width, cost，
+        从一个start_width（包含）到另一个start_width（不包含）为止的原料的成本为cost
+        :return: None
+        """
+        # 首先进行排序
+        cost_df.sort_values(by="start_width", axis=0, inplace=True, ignore_index=True)
 
-        # 计算每种组合的总宽度
-        whole_width = temp_df.values.dot(products.width.values.reshape(-1, 1))
-
-        # 筛选无边丝的方案
-        filtered_df = temp_df[whole_width.flatten() == raw_materials.loc[r, "width"]]
-
-        if len(filtered_df) > 0:
-            pattern_matrices[raw_materials.loc[r, 'width']] = filtered_df.reset_index(drop=True)
-
-    return pattern_matrices
+        pass
 
 
-def generate_cutting_patterns_with_redundancy(raw_materials, products):
-    """
-    处理价格区间边缘位置的原料的方案，此时允许有边丝，但是边丝不得超过成品宽度的最小值
-    （否则显然有更佳的方案）。
-    arguments:
-    - raw_materials: DataFrame，包含 "width"（原材料宽度）
-    - products: DataFrame，包含 "width"（成品宽度）
-    - width_cost: DataFrame, 包含"width"(区间最小宽度,区间包含此宽度), "cost":成本。
+    @staticmethod
+    def merge(one, another):
+        """
+        合并两个 CuttingPatterns 的裁剪方案。
+        要求两个对象的成品宽度必须一致。
+        """
+        if not one.products.width.equals(another.products.width):
+            raise ValueError("合并方案要求成品宽度一致")
 
-    return:
-    - pattern_matrices: 一个dict，每个元素是一个 DataFrame（表示该原材料的所有裁剪方案）
-    """
-    max_counts = (raw_materials.width.values.reshape(-1, 1) //
-                  products.width.values.reshape(1, -1))
-    pattern_matrices = {}
-    for r in range(len(raw_materials.width)):
-        # 生成所有组合
-        temp = itertools.product(*[range(count + 1) for count in max_counts[r]])
-        temp_df = pd.DataFrame(temp, columns=products.width)
+        # 复制当前对象的raw_matrices，避免修改原数据
+        merged_matrices = one.raw_matrices.copy()
 
-        # 计算每种组合的总宽度
-        whole_width = temp_df.values.dot(products.width.values.reshape(-1, 1))
+        for raw_width, m2_df in another.raw_matrices.items():
+            if raw_width in merged_matrices:
+                # 合并两个 DataFrame，去重
+                merged_matrices[raw_width] = pd.concat(
+                    [merged_matrices[raw_width], m2_df]
+                                                      ).drop_duplicates(ignore_index=True)
+            else:
+                merged_matrices[raw_width] = m2_df
 
-        # 筛选边丝宽度不超过最小成品宽度的
-        filtered_df = temp_df[(whole_width.flatten() <= raw_materials.loc[r, "width"]) &
-                              (whole_width.flatten() >= (raw_materials.loc[r, "width"] - products.width.min()))]
+        result = CuttingPatterns(one.products)
+        result.raw_matrices = merged_matrices
+        return result
 
-        if len(filtered_df) > 0:
-            pattern_matrices[raw_materials.loc[r, 'width']] = filtered_df.reset_index(drop=True)
-    return pattern_matrices
+    def generate_without_redundancy(self, raw_materials):
+        """
+        生成无边丝（无冗余宽度）的裁剪方案
 
+        arguments：
+        - raw_materials: DataFrame，包含 "width"（原材料宽度）
+        - products: DataFrame，包含 "width"（成品宽度）
 
+        return：
+        - pattern_matrices: 一个列表，每个元素是一个 DataFrame（表示该原材料的所有裁剪方案）
+        """
+        max_counts = (raw_materials.width.values.reshape(-1, 1) //
+                      self.products.width.values.reshape(1, -1))
+        pattern_matrices = {}
+        for r in range(len(raw_materials.width)):
+            # 生成所有组合
+            temp = itertools.product(*[range(count + 1) for count in max_counts[r]])
+            temp_df = pd.DataFrame(temp, columns=self.products.width)
+
+            # 计算每种组合的总宽度
+            whole_width = temp_df.values.dot(self.products.width.values.reshape(-1, 1))
+
+            # 筛选无边丝的方案
+            filtered_df = temp_df[whole_width.flatten() == raw_materials.loc[r, "width"]]
+
+            if len(filtered_df) > 0:
+                pattern_matrices[raw_materials.loc[r, 'width']] = filtered_df.reset_index(drop=True)
+
+        return pattern_matrices
+
+    def generate_with_redundancy(self, raw_materials):
+        """
+        处理价格区间边缘位置的原料的方案，此时允许有边丝，但是边丝不得超过成品宽度的最小值
+        （否则显然有更佳的方案）。
+        arguments:
+        - raw_materials: DataFrame，包含 "width"（原材料宽度）
+        - products: DataFrame，包含 "width"（成品宽度）
+        - width_cost: DataFrame, 包含"width"(区间最小宽度,区间包含此宽度), "cost":成本。
+
+        return:
+        - pattern_matrices: 一个dict，每个元素是一个 DataFrame（表示该原材料的所有裁剪方案）
+        """
+        max_counts = (raw_materials.width.values.reshape(-1, 1) //
+                      self.products.width.values.reshape(1, -1))
+        pattern_matrices = {}
+        for r in range(len(raw_materials.width)):
+            # 生成所有组合
+            temp = itertools.product(*[range(count + 1) for count in max_counts[r]])
+            temp_df = pd.DataFrame(temp, columns=self.products.width)
+
+            # 计算每种组合的总宽度
+            whole_width = temp_df.values.dot(self.products.width.values.reshape(-1, 1))
+
+            # 筛选边丝宽度不超过最小成品宽度的
+            filtered_df = temp_df[(whole_width.flatten() <= raw_materials.loc[r, "width"]) &
+                                  (whole_width.flatten() >= (
+                                              raw_materials.loc[r, "width"] - self.products.width.min()))]
+
+            if len(filtered_df) > 0:
+                pattern_matrices[raw_materials.loc[r, 'width']] = filtered_df.reset_index(drop=True)
+        return pattern_matrices
 
 
 def solve():
@@ -94,7 +140,7 @@ def solve():
             l[(i, j)] = solver.NumVar(0, solver.Infinity(), f"l[{i}, {j}]")
 
     # 定义目标函数
-    objective = solver.Objective()  #目标为使用原材料的总面积最小
+    objective = solver.Objective()  # 目标为使用原材料的总面积最小
     for (i, j), var in l.items():
         objective.SetCoefficient(var, float(raw_materials.loc[i, "width"]))
     objective.SetMinimization()
@@ -108,20 +154,20 @@ def solve():
             coeff = float(pattern_matrices[i].iloc[j, p])
             constraint.SetCoefficient(var, coeff)
 
-
     # 求解
     status = solver.Solve()
 
     # 输出结果
     if status == pywraplp.Solver.OPTIMAL:
         print("已找到最优解！")
-        col= [products.loc[idx, "width"] for idx in range(len(products))] + ["len_used"]
-        result = pd.DataFrame(columns = col)
+        col = [products.loc[idx, "width"] for idx in range(len(products))] + ["len_used"]
+        result = pd.DataFrame(columns=col)
         for (i, j), var in l.items():
             if var.solution_value() > 0:
-                pattern_content = pattern_matrices[i].iloc[j]   #当前pattern的内容
+                pattern_content = pattern_matrices[i].iloc[j]  # 当前pattern的内容
                 result.loc[len(result)] = list(pattern_content) + [var.solution_value()]
 
-
     return result
+
+
 solve()
