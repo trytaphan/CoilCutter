0302ec2a97732343f370bba3070fe7090cfdd46f
Xuan <893570476@qq.com>
2025-02-14 11:26:25 +0800

    正在写ui

--- support.py
+++ support.py
@@ -1,12 +1,9 @@
 import pandas as pd
 
-
-
-
 class SupportBracket:
     property_list = ["shape", "height", "dimension_B", "dimension_C", "thickness",
                      "length", "specification", "height_r", "dimension_B_r",
-                     "dimension_C_r", "thickness_r", "specification_r"]
+                     "dimension_C_r", "thickness_r", "specification_r", "count"]
 
     chinese_mapping = {
         "shape": "形状",
@@ -20,13 +17,17 @@ class SupportBracket:
         "dimension_B_r": "实际宽度B",
         "dimension_C_r": "实际宽度C",
         "thickness_r": "实际厚度",
-        "specification_r": "实际规格"
+        "specification_r": "实际规格",
+        "count": "数量"
     }
 
-    def __init__(self, specification: str, count) -> None:
+    def __init__(self, is_empty=False, count=0, specification=None):
         """
         specification: 规格，如C100*35*10*2.5*9775
         """
+        if is_empty:
+            return
+
         # 解析规格
         specification = specification.replace(" ", "")
         self.shape = specification[0]
@@ -37,6 +38,9 @@ class SupportBracket:
         self.thickness = float(spec_dict[3])
         self.length = float(spec_dict[4])
         self.specification = specification
+
+        # 其他属性
+        self.count = count
 
     # 实际生产规格
     @property
@@ -70,18 +74,50 @@ class SupportBracket:
 
     @staticmethod
     def prepare_products(brackets):
-        for bracket
+        pass
+
+    @staticmethod
+    def load_from_dataframe(df, idx):
+        """"从具备全部属性的dataframe里加载"""
+        inst = SupportBracket(empty=True)
+        for prop in SupportBracket.property_list:
+            setattr(inst, prop, df.loc[idx, str(prop)])
+        return inst
+
+
+class Brackets:
+    def __init__(self):
+        self.brackets = []
+
+    def add_bracket(self, bracket:SupportBracket):
+        """添加一个新的 bracket 到列表"""
+        self.brackets.append(bracket)
+
+    def remove_bracket(self, idx:int):
+        """删除指定索引的bracket"""
+        if 0 <= idx < len(self.brackets):
+            del self.brackets[idx]
+
+    def to_dataframe(self):
+        """转换为 DataFrame"""
+        data = [bracket.to_dict() for bracket in self.brackets]
+        return pd.DataFrame(data, columns = SupportBracket.property_list)
+
+    @staticmethod
+    def load_from_dataframe(df):
+        """从DataFrame加载"""
+        return [SupportBracket.load_from_dataframe(df, idx) for idx in df.index]
 
 
 if __name__ == '__main__':
-    a = SupportBracket(" C 100*35*10*2.5*9775")
-    b = SupportBracket("H200*150*3.2*4.5*2000")
-    d = SupportBracket("C100*35*10*2.5*9775")
+    a = SupportBracket(" C 100*35*10*2.5*9775", 5)
+    # b = SupportBracket("H200*150*3.2*4.5*2000")
+    # d = SupportBracket("C100*35*10*2.5*9775")
     print(a.to_dict())
     df = pd.DataFrame(columns=SupportBracket.property_list)
-    print(len(df.index))
-    df.loc[len(df.index)] = a.to_dict()
-    df.loc[len(df.index)] = b.to_dict()
+    # print(len(df.index))
+    # df.loc[len(df.index)] = a.to_dict()
+    # df.loc[len(df.index)] = b.to_dict()
 
     print(df)
     print(a.specification)
