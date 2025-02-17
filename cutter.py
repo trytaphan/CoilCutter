472a68c27366e1bc89a3e83e5df9ffbf1333c1c8
Xuan <893570476@qq.com>
2025-02-17 14:48:33 +0800

    正在写ui

--- cutter.py
+++ cutter.py
@@ -1,30 +1,44 @@
-import streamlit as st
-import pandas as pd
 from st_aggrid import AgGrid, GridOptionsBuilder
-from support import SupportBracket
+from support import *
+from solution import *
+from pathlib import Path
+from support import display_in_Chinese
 
-st.title("Cutter")
-a = SupportBracket(" C 100*35*10*2.5*9775")
-b = SupportBracket("H200*150*3.2*4.5*2000")
-d = SupportBracket("C100*35*10*2.5*9775")
-print(a.to_dict())
-df = pd.DataFrame(columns=SupportBracket.property_list)
-print(len(df.index))
-df.loc[len(df.index)] = a.to_dict()
-df.loc[len(df.index)] = b.to_dict()
-edited_df = st.data_editor(SupportBracket.display_in_CN(df))
-# 配置GridOptions
-gb = GridOptionsBuilder.from_dataframe(df)
-gb.configure_selection(selection_mode="multiple",    # 允许多选行
-                       use_checkbox=True)            # 加勾选框
-gb.configure_grid_options(enableRangeSelection=True)    # 框选多个单元格，支持shift和ctrl
-grid_options = gb.build()
+st.set_page_config(page_title="带钢裁剪系统", layout="wide")
+st.title("📏 带钢裁剪系统")
 
+# 下载标准模板
+template_path = Path(r"assets/input_template.xlsx")
+with open(template_path, "rb") as template:
+    st.download_button(
+        label="下载模板",
+        data=template,
+        file_name="模版.xlsx",
+        mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        icon="📥",
+        help="请下载、填写该模板后上传数据"
 
-grid_response = AgGrid(df,
-                       gridOptions=grid_options,
-                       editable=True,
-                       height=100
-                       )
-# st.dataframe(df)
+    )
+# 再次上传已经填好的数据
+uploaded_file = st.file_uploader(label="📂 上传数据",
+                                 type=["xlsx"],
+                                 help="请按上面的模板上传数据")
+if uploaded_file:
+    # 解析
+    brackets = Brackets.from_excel(uploaded_file, re_parse=True)
+    df = brackets.to_dataframe()
 
+    # 选定需要展示的列, 并翻译
+    columns = ["name", "specification", "grade", "count", "specification_t"]
+    filtered_df = display_in_Chinese(df[columns])
+    # 配置GridOptions
+    gb = GridOptionsBuilder.from_dataframe(filtered_df)
+    gb.configure_selection(selection_mode="multiple",  # 允许多选行
+                           use_checkbox=True)  # 加勾选框
+    gb.configure_grid_options(enableRangeSelection=True)  # 框选多个单元格，支持shift和ctrl
+    grid_options = gb.build()
+    grid_response = AgGrid(filtered_df,
+                           gridOptions=grid_options,
+                           editable=True,
+                           height=max(50 * len(filtered_df), 300)
+                           )
--- support.py
+++ support.py
@@ -1,11 +1,14 @@
 import pandas as pd
+import streamlit as st
+import openpyxl
+
 
 class SupportBracket:
     property_list = ["shape", "height", "dimension_B", "dimension_C", "thickness",
-                     "length", "specification", "height_r", "dimension_B_r",
-                     "dimension_C_r", "thickness_r", "specification_r", "count"]
+                     "length", "specification", "height_t", "dimension_B_t",
+                     "dimension_C_t", "thickness_t", "specification_t", "count", "name", "grade"]
 
-    chinese_mapping = {
+    English_Chinese_mapping = {
         "shape": "形状",
         "height": "高度",
         "dimension_B": "宽度B",
@@ -13,23 +16,55 @@ class SupportBracket:
         "thickness": "厚度",
         "length": "长度",
         "specification": "规格",
-        "height_r": "实际高度",
-        "dimension_B_r": "实际宽度B",
-        "dimension_C_r": "实际宽度C",
-        "thickness_r": "实际厚度",
-        "specification_r": "实际规格",
-        "count": "数量"
+        "height_t": "目标高度",
+        "dimension_B_t": "目标宽度B",
+        "dimension_C_t": "目标宽度C",
+        "thickness_t": "目标厚度",
+        "specification_t": "目标规格",
+        "count": "总数量",
+        "grade": "材质",
+        "name": "名称"
     }
+    Chinese_English_mapping = \
+        {chinese: english for english, chinese in English_Chinese_mapping.items()}
 
-    def __init__(self, is_empty=False, count=0, specification=None):
+    # 检查一下是不是都有翻译
+    if not len(property_list) == len(Chinese_English_mapping):
+        raise ValueError("SupportBrackets: 有属性未翻译或者有属性没加入property_list！")
+    for prop in property_list:
+        if not prop in English_Chinese_mapping:
+            raise ValueError("SupportBrackets: 有属性未翻译！")
+
+
+    def __init__(self, count=0, specification=None):
         """
         specification: 规格，如C100*35*10*2.5*9775
         """
-        if is_empty:
-            return
+        if specification is None:
+            # 为空对象设置默认值
+            self.shape = ""
+            self.height = 0
+            self.dimension_B = 0
+            self.dimension_C = 0
+            self.thickness = 0
+            self.length = 0
+            self.specification = ""
+        else:
+            # 解析规格
+            self.parse_specification(specification)
+            # 计算目标尺寸
+            self.init_target_dimensions()
 
-        # 解析规格
-        specification = specification.replace(" ", "")
+        # 其他属性
+        self.count = count
+
+    def parse_specification(self, specification=None):
+        """解析规格"""
+        if specification is not None:
+            specification = specification.replace(" ", "")
+        else:
+            specification = self.specification
+        SupportBracket.check_specification(specification)
         self.shape = specification[0]
         spec_dict = specification[1:].split(sep="*")
         self.height = float(spec_dict[0])
@@ -39,87 +74,119 @@ class SupportBracket:
         self.length = float(spec_dict[4])
         self.specification = specification
 
-        # 其他属性
-        self.count = count
+    def init_target_dimensions(self):
+        """目标尺寸：实际生产时要达到的尺寸"""
+        self.height_t = self.height - 1
+        self.dimension_B_t = self.dimension_B - 1
+        self.dimension_C_t = self.dimension_C - 0.5
+        self.thickness_t = self.thickness
+        self.specification_t = (f"{self.shape}{self.height_t}*{self.dimension_B_t}*{self.dimension_C_t}"
+                                f"*{self.thickness_t}*{self.length}")
 
-    # 实际生产规格
-    @property
-    def height_r(self):
-        return self.height - 1
-
-    @property
-    def dimension_B_r(self):
-        return self.dimension_B - 1
-
-    @property
-    def dimension_C_r(self):
-        return self.dimension_C - 0.5
-
-    @property
-    def thickness_r(self):
-        return self.thickness
-
-    @property
-    def specification_r(self):
-        """返回实际规格字符串"""
-        return (f"{self.shape}{self.height_r}*{self.dimension_B_r}*{self.dimension_C_r}"
-                f"*{self.thickness_r}*{self.length}")
+    def update_target_dimensions(self, spec_t=None):
+        """利用已有的目标规格计算其他属性"""
+        if spec_t is not None:
+            spec_t = spec_t.replace(" ", "")
+        else:
+            spec_t = self.specification_t
+        SupportBracket.check_specification(spec_t)
+        spec_dict = spec_t[1:].split(sep="*")
+        self.height_t = float(spec_dict[0])
+        self.dimension_B_t = float(spec_dict[1])
+        self.dimension_C_t = float(spec_dict[2])
+        self.thickness_t = float(spec_dict[3])
+        self.specification_t = spec_t
 
     def to_dict(self):
-        return {str(prop):getattr(self, prop) for prop in self.property_list}
+        return {str(prop): getattr(self, prop) for prop in self.property_list}
+
 
     @staticmethod
-    def display_in_CN(df):
-        return df.rename(columns = SupportBracket.chinese_mapping)
+    def check_specification(specification):
+        spec = specification.replace(" ", "")
+        try:
+            if spec.count("*") != 4:
+                raise ValueError(r"*不是4个。")
+            if not spec.startswith(("C")):
+                raise ValueError("不是以C开头。")
+        except Exception as e:
+            print(f"请检查规格{spec}: {e}")
+            st.warning(f"请检查规格{spec}: {e}")
 
     @staticmethod
     def prepare_products(brackets):
         pass
 
     @staticmethod
-    def load_from_dataframe(df, idx):
-        """"从具备全部属性的dataframe里加载"""
-        inst = SupportBracket(empty=True)
-        for prop in SupportBracket.property_list:
-            setattr(inst, prop, df.loc[idx, str(prop)])
+    def from_dataframe(df, idx, re_parse=False):
+        """用指定DataFrame的idx行填充实例属性"""
+        inst = SupportBracket()
+        for prop in df.columns:
+            setattr(inst, prop, df.loc[idx, prop])
+        if re_parse:
+            inst.parse_specification()
+            inst.init_target_dimensions()
         return inst
 
 
 class Brackets:
     def __init__(self):
-        self.brackets = []
+        self.list = []
 
-    def add_bracket(self, bracket:SupportBracket):
+    def add_bracket(self, bracket: SupportBracket):
         """添加一个新的 bracket 到列表"""
-        self.brackets.append(bracket)
+        self.list.append(bracket)
+        return self
 
-    def remove_bracket(self, idx:int):
+    def remove_bracket(self, idx: int):
         """删除指定索引的bracket"""
-        if 0 <= idx < len(self.brackets):
-            del self.brackets[idx]
+        if 0 <= idx < len(self.list):
+            del self.list[idx]
+        else:
+            st.warning(f"remove_bracket:传入的索引{idx}不在目标范围内！")
 
     def to_dataframe(self):
         """转换为 DataFrame"""
-        data = [bracket.to_dict() for bracket in self.brackets]
-        return pd.DataFrame(data, columns = SupportBracket.property_list)
+        data = [bracket.to_dict() for bracket in self.list]
+        return pd.DataFrame(data)
 
     @staticmethod
-    def load_from_dataframe(df):
+    def from_dataframe(df, re_parse=False):
         """从DataFrame加载"""
-        return [SupportBracket.load_from_dataframe(df, idx) for idx in df.index]
+        df = df.fillna(0)
+        b = Brackets()
+        b.list = [SupportBracket.from_dataframe(df, idx, re_parse) for idx in df.index]
+        return b
+
+    @staticmethod
+    def from_csv(file_path):
+        df = pd.read_csv(file_path)
+        return Brackets.from_dataframe(df)
+
+    def to_csv(self, file_path):
+        df = self.to_dataframe()
+        df.to_csv(file_path, index=False)
+
+    @staticmethod
+    def from_excel(file_path, re_parse=False):
+        df = pd.read_excel(file_path, engine="openpyxl")
+        df = display_in_English(df)
+        return Brackets.from_dataframe(df, re_parse)
+
+
+
+def display_in_Chinese(df):
+    return df.rename(columns=SupportBracket.English_Chinese_mapping)
+
+
+def display_in_English(df):
+    return df.rename(columns=SupportBracket.Chinese_English_mapping)
 
 
 if __name__ == '__main__':
-    a = SupportBracket(" C 100*35*10*2.5*9775", 5)
-    # b = SupportBracket("H200*150*3.2*4.5*2000")
-    # d = SupportBracket("C100*35*10*2.5*9775")
-    print(a.to_dict())
-    df = pd.DataFrame(columns=SupportBracket.property_list)
-    # print(len(df.index))
-    # df.loc[len(df.index)] = a.to_dict()
-    # df.loc[len(df.index)] = b.to_dict()
+    from pathlib import Path
 
-    print(df)
-    print(a.specification)
-    print(a.specification_r)
-    print(SupportBracket.display_in_CN(df))
+    path = Path(r"D:\project\coilCutter\assets\input_template.xlsx")
+    bs = Brackets()
+    bs.from_excel(path)
+    bs.to_dataframe()
